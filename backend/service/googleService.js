1af13a83f9530bd9a4f28f5e7e451a4cc9607b6c
Ayush-desaii <110298454+Ayush-desaii@users.noreply.github.com>
2025-04-09 17:23:03 +0530

    google auth done

--- backend/service/googleService.js
+++ backend/service/googleService.js
@@ -2,7 +2,7 @@ import { google } from "googleapis";
 class GoogleService {
     constructor(authService) {
         this.authService = authService;
-        this.googleClient = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI);
+        this.googleClient = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URI);
     }
 
     async gAuth() {
@@ -20,17 +20,17 @@ class GoogleService {
     async authenticateCallback(code) {
 
         const { tokens } = await this.googleClient.getToken(code)
-        googleClient.setCredentials(tokens)
+        this.googleClient.setCredentials(tokens)
 
         const oauth2 = google.oauth2({ version: "v2", auth: this.googleClient });
         const { data } = await oauth2.userinfo.get();
 
-        Username = data.name;
-        Full_Name = data.name;
-        email = data.email;
-        password = "google";
+        const Username = data.name;
+        const Full_Name = data.name;
+        const email = data.email;
+        const password = "google";
 
-        const user = await this.authService.register(Username, Full_Name, email, password);
+        const user = await this.authService.registerGoogle(Username, Full_Name, email, password);
         return user;
     }
 }
