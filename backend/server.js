a2be003585c78d27e8c455de0f97f7a95821f354
aryan <aryanjadav081103@gmail.com>
2025-04-10 10:22:21 +0530

    added url in cors origin

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(CookieParser()); 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://invideo-eta.vercel.app/",
     credentials: true,
   })
 );
